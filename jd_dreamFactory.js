74ae4cd9137e76210049b34d1372aba44d1b29ac
androidJiam <jiamws@163.com>
2021-01-26 21:14:39 +0800

    Update jd_dreamFactory.js

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -34,7 +34,7 @@ const randomCount = $.isNode() ? 20 : 5;
 let tuanActiveId = `6S9y4sJUfA2vPQP6TLdVIQ==`;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '';
-const inviteCodes = ['Zn4t2HPyVqormcJg6WD9lg==@7h5nFyw2oLJ_jz9AonJwVA==', '7h5nFyw2oLJ_jz9AonJwVA=='];
+const inviteCodes = ['Zn4t2HPyVqormcJg6WD9lg==@7h5nFyw2oLJ_jz9AonJwVA==', 'Zn4t2HPyVqormcJg6WD9lg=='];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
